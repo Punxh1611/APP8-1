d81160b6ba24dbc7002a6a4b6354701f7f423045
Punxh <punch8319@gmail.com>
2026-09-21 18:04:10 +0700

    REST API information

--- server/index.js
+++ server/index.js
@@ -45,6 +45,22 @@ app.get('/api/form-get', (request, response) => {
     response.json(r)
 })
 
+app.post('/api/form-post', (request, response) => {
+    let name = request.body.name || ''
+    let email = request.body.email || ''
+    let msg = request.body.message || ''
+
+    let text = `
+    <table border="1">
+    <caption>ข้อมูลที่ส่งขึ้นไป</caption>
+    <tr><td>ชื่อ:</td><td>${name}</td></tr>
+    <tr><td>อีเมล:</td><td>${email}</td></tr>
+    <tr><td>ข้อความ:</td><td>${msg}</td></tr>
+    </table>
+    `
+    response.send(text)
+})
+
 app.listen(port, () => {
     console.log('Server listening on port ' + port)
 })
